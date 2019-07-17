f63e9ba37d869c76480c6e9ec0fe1bedd1b63ce0
xiaoqiang.djq <xiaoqiang.djq@alibaba-inc.com>
2019-07-17 21:30:25 +0800

    update

---
+++ src/pages/list/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="outer">
+        <ul>
+            <li :key="item" v-for="item in list">{{item}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+    },
+    data() {
+        return {
+            list : [
+                1,2,3,4
+            ]
+        }
+    },
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="less">
+html, body {
+    margin: 0;
+}
+</style>
