fd74559d0bfb54c049f4b0ac6f9ad556005af749
xiaoqiang.djq <xiaoqiang.djq@alibaba-inc.com>
2019-08-28 22:18:43 +0800

    proxy

---
+++ build/common.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{14:function(o,n,s){(o.exports=s(1)(!1)).push([o.i,"",""])},8:function(o,n,s){var i=s(14);"string"==typeof i&&(i=[[o.i,i,""]]);var p={hmr:!0,transform:void 0,insertInto:void 0};s(2)(i,p);i.locals&&(o.exports=i.locals)}}]);
---
+++ src/pages/error/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{message}}</h1>
+</template>
+
+<script>
+import '../../common.less';
+
+export default {
+    components: {
+    },
+    data() {
+        return {
+            message : '404'
+        }
+    },
+    methods: {
+        
+    },
+};
+</script>
